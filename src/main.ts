98ed76f8d4264a4fb8ce97d35eb8fa9e7ffb7e9b
Prakash Pandey <pkpndy@gmail.com>
2024-05-23 07:25:03 +0530

    added edit packages and flags

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,24 @@ const readline = require('readline');
 const util = require('util');
 import PackageModel from './models/package.model';
 import FlagsModel, { FlagsClass } from './models/flags.model';
-import { Ref } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
+
+interface PackageUpdateDetails {
+    packageId: string;
+    newName?: string;
+    addFlags?: string[]; 
+    removeFlags?: string[]; 
+}
+
+interface FlagUpdateDetails {
+    flagId: string;
+    newName?: string;
+    newSummary?: string;
+    newValue?: string | number;
+    addPackages?: string[];
+    removePackages?: string[];
+}
+
 
 interface PackageDetails {
     packageName: string;
@@ -37,7 +54,8 @@ const run = async () => {
     });
 
     const packageDetails = {
-        packageName: "NewPackage11"
+        packageName: "NewPackage010",
+        flagsAssociated: ["6649ef49a7f334b2d142d9c4"]
     };
 
     // const newPackage = await createPackage(packageDetails);
@@ -45,13 +63,24 @@ const run = async () => {
     // await getAllPackages();
 
     const flagDetails = {
-        flagName: "newwwwwwwFlagg",
-        summary: "hey giys this is my new flag",
-        flagValue: 0,
-        packagesAssociated: ["6649e4e9f6bbf596b10788eb"],
+        flagName: "newwwFlagg",
+        summary: "hey giys this is flag",
+        flagValue: 3,
     }
 
     // await createFlag(flagDetails);
+
+    const packageUpdateDetails = {
+        packageId: "664e9cb52fcf951361a99516",
+        addFlags: ["664e20a07361894f91510268"],
+        removeFlags: ["6649ef49a7f334b2d142d9c4"]
+    }
+
+    // exampleFunction("value1", param3: "value3", param4: "value4");
+    // exampleFunction({ param1: "value1", param3: "value3", param4: "value4" });
+    // await editPackage(packageUpdateDetails);
+
+    // await editFlag({ flagId : "6649ef49a7f334b2d142d9c4", newSummary: "lol rcb just got eliminated", addPackages: ["664e9cb52fcf951361a99516"], removePackages: ["664e23d196da3acbb0613e03"] });
 
     // await getPackaDetailsByPackageName("NewPackage11");
 
@@ -64,9 +93,50 @@ const run = async () => {
 
     // await deleteFlag("newwwwwwwFlagg");
 
-    await deletePackage("NewPackage11");
+    // await deletePackage("NewPackage11");
 
     await mongoose.disconnect();
+};
+
+const editPackage = async ({ packageId, newName, addFlags = [], removeFlags = [] }: PackageUpdateDetails) => {
+    const packageDoc = await PackageModel.findById(packageId).exec();
+
+    if (packageDoc) {
+         // Update the package name if provided
+        if (newName) {
+            packageDoc.packageName = newName;
+        }
+
+        // Remove specified flags from the package
+        if (removeFlags.length > 0 && packageDoc.flagsAssociated !== undefined) {
+            packageDoc.flagsAssociated = packageDoc.flagsAssociated.filter(flag => !removeFlags.includes(flag.toString()));
+            // Remove the package reference from the removed flags
+            await FlagsModel.updateMany(
+                { _id: { $in: removeFlags } },
+                { $pull: { packagesAssociated: packageDoc._id.toString() } }
+            ).exec();
+        }
+
+        // Add specified flags to the package
+        if (addFlags.length > 0 && packageDoc.flagsAssociated !== undefined) {
+            packageDoc.flagsAssociated.push(...addFlags.map(id => new Types.ObjectId(id)));
+        // Add the package reference to the added flags
+        await FlagsModel.updateMany(
+            { _id: { $in: addFlags.map(id => new Types.ObjectId(id)) } }, // Convert IDs to ObjectIds
+            { $addToSet: { packagesAssociated: packageDoc._id.toString() } }
+        ).exec();
+        }
+
+        // Save the updated package document
+        await packageDoc.save();
+
+        console.log('Package updated successfully.');        
+    }
+    else {
+        console.log('Package not found.');
+        return;
+    }
+
 };
 
 const createPackage = async (packageDetails: PackageDetails) => {
@@ -102,6 +172,56 @@ const createFlag = async (flagDetails: FlagDetails) => {
     console.log(`Flag created:\n ${newFlag.flagName} `);
 }
 
+const editFlag = async ({ flagId, newName, newSummary, newValue, addPackages = [], removePackages = [] }: FlagUpdateDetails) => {
+    const flagDoc = await FlagsModel.findById(flagId).exec();
+
+    if (!flagDoc) {
+        console.log('Flag not found.');
+        return;
+    }
+
+    // Update the flag name if provided
+    if (newName) {
+        flagDoc.flagName = newName;
+    }
+
+    // Update the flag summary if provided
+    if (newSummary) {
+        flagDoc.summary = newSummary;
+    }
+
+    // Update the flag value if provided
+    if (newValue !== undefined) {
+        flagDoc.flagValue = newValue;
+    }
+
+    // Remove specified packages from the flag
+    if (removePackages.length > 0 && flagDoc.packagesAssociated != undefined) {
+        flagDoc.packagesAssociated = flagDoc.packagesAssociated.filter(pkg => !removePackages.includes(pkg.toString()));
+        // Remove the flag reference from the removed packages
+        await PackageModel.updateMany(
+            { _id: { $in: removePackages } },
+            { $pull: { flagsAssociated: flagDoc._id.toString() } }
+        ).exec();
+    }
+
+    // Add specified packages to the flag
+    if (addPackages.length > 0 && flagDoc.packagesAssociated != undefined) {
+        flagDoc.packagesAssociated.push(...addPackages.filter(pkg => !flagDoc.packagesAssociated!.includes(pkg as any)));
+        // Add the flag reference to the added packages
+        await PackageModel.updateMany(
+            { _id: { $in: addPackages } },
+            { $addToSet: { flagsAssociated: flagDoc._id.toString() } }
+        ).exec();
+    }
+
+    // Save the updated flag document
+    await flagDoc.save();
+
+    console.log('Flag updated successfully.');
+};
+
+
 const getAllPackages = async () => {
     const res = await PackageModel.find().populate('flagsAssociated').exec();
     if (!res || res.length === 0) {
@@ -118,14 +238,6 @@ const getPackaDetailsByPackageName = async (packageName: String) => {
     } else {
         console.log(util.inspect(res, { showHidden: false, depth: null, colors: true }));
     }
-}
-
-const getFlagIDsFromFlagNames = async (flagNames: string[]): Promise<string[]> => {
-    const flags = await FlagsModel.find({ flagName: { $in: flagNames } }, { _id: 1 });
-
-    // Extract and return the IDs of the flags
-    const flagIDs = flags.map(flag => flag._id.toString());
-    return flagIDs;
 }
 
 const getFlagDetailsByFlagName = async (flagName: string) => {
